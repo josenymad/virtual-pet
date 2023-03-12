24c324591782a4974a554822b6275124b9483cfb
Jose Wills <jose.wills3@gmail.com>
2023-03-12 09:24:49 +0000

    passed growUp

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -13,4 +13,10 @@ describe('constructor', () => {
     it('pet has an initial age of 0', () => {
         expect(new Pet('Lucy').age).toBe(0);
     });
+
+    it('growUp', () => {
+        const pet = new Pet('Lucky');
+        pet.growUp();
+        expect(pet.age).toBe(1);
+    });
 });
--- src/pet.js
+++ src/pet.js
@@ -1,6 +1,9 @@
 function Pet(petName) {
-    this.name = petName,
-    this.age = 0
+    this.name = petName;
+    this.age = 0;
+    this.growUp = function() {
+        this.age += 1;
+    }
 };
 
 module.exports = Pet;
